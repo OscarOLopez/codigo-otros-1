ad54454c074aea31ae4037bef309d8d67f6c0592
OscarOLopez <oscar.lop.lic@gmail.com>
2024-11-05 17:28:31 -0600

    Correcciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+// Se corrige la llamada a la clase name 
+const $n = document.querySelector('.name');
+// Se corrige la llamada a la clase blog
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//Se agrega async a la funcion  para que pueda esperar a que se resuelva la promesa
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  // Se agrega la constante data y response se convierte a json 
+  const data = await  response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //Se cambian las comillas dobles por backticks de las tres asignaciones de valores
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
